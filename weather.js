f97df6af26c218d410ddf1c014a1a4f6f3e403f7
Arinil Haq <arinil.haq01@ui.ac.id>
2023-06-14 22:16:16 +0700

    Trying forecast 5 days

--- weather.js
+++ weather.js
@@ -153,4 +153,72 @@ document.querySelector(".weather_unit_farenheit").addEventListener('click', () =
     }
 })
 
-document.body.addEventListener('load', getWeatherByCity(currCity))
+
+function GetInfo() {
+
+    var newName = document.getElementById("cityInput");
+    var cityName = document.getElementById("cityName");
+    cityName.innerHTML = "--"+newName.value+"--";
+
+fetch('https://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appid=d2c621e47181ff427c2d3fe67c0b877a')
+.then(response => response.json())
+.then(data => {
+
+    //Getting the min and max values for each day
+    for(i = 0; i<5; i++){
+        document.getElementById("day" + (i+1) + "Min").innerHTML = "Min: " + Number(data.list[i].main.temp_min - 273.15).toFixed(1)+ "°";
+        //Number(1.3450001).toFixed(2); // 1.35
+    }
+
+    for(i = 0; i<5; i++){
+        document.getElementById("day" + (i+1) + "Max").innerHTML = "Max: " + Number(data.list[i].main.temp_max - 273.15).toFixed(2) + "°";
+    }
+    //------------------------------------------------------------
+
+    //Getting Weather Icons
+     for(i = 0; i<5; i++){
+        document.getElementById("img" + (i+1)).src = "http://openweathermap.org/img/wn/"+
+        data.list[i].weather[0].icon
+        +".png";
+    }
+    //------------------------------------------------------------
+    console.log(data)
+
+    for(i = 0; i<5; i++){
+        document.getElementById("day" + (i+1)).innerHTML = weekday[CheckDay(i)];
+    }
+
+
+})
+
+.catch(err => alert("Something Went Wrong: Try Checking Your Internet Coneciton"))
+}
+
+function DefaultScreen(){
+    document.getElementById("cityInput").defaultValue = "London";
+    GetInfo();
+}
+
+
+//Getting and displaying the text for the upcoming five days of the week
+var d = new Date();
+var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",];
+
+//Function to get the correct integer for the index of the days array
+function CheckDay(day){
+    if(day + d.getDay() > 6){
+        return day + d.getDay() - 7;
+    }
+    else{
+        return day + d.getDay();
+    }
+}
+
+// for(i = 0; i<5; i++){
+//     document.getElementById("day" + (i+1)).innerHTML = weekday[CheckDay(i)];
+// }
+//------------------------------------------------------------
+
+
+
+document.body.addEventListener('load', getWeatherByCity(currCity), GetInfo())
